a2fdcf98a1a39275538936b9c5543d3dd0da972a
C Andrew Byrd IV <cabyrd4@gmail.com>
2016-08-27 20:26:10 -0400

    first commit

---
+++ Exploring.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+
+shots = read.csv("shot_logs.csv", stringsAsFactors=FALSE)
+
+str(shots)
+summary(shots)
+
+ggplot(shots, aes(x=SHOT_DIST, color=SHOT_RESULT)) +
+         geom_density()
